d0f78235349e83cffdba9002352cc2d32a01cde6
Twanghofer <tobias.w01@gmx.at>
2019-09-25 12:18:43 +0200

    Added a SceneSwitcher Button and Script

---
+++ MEP_Project1/Assets/SwitchScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SwitchScene : MonoBehaviour
+{
+    string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+    }
+
+    public void switchScene()
+    {
+        if (sceneName == "Scene1")
+        {
+            SceneManager.LoadScene("Scene2");
+        }
+       
+        else
+        {
+            SceneManager.LoadScene("Scene1");
+        }
+    }
+}
