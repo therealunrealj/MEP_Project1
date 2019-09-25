9daa7f4ea611ec138960f68293f1d5521c90f7b4
Twanghofer <tobias.w01@gmx.at>
2019-09-25 12:36:51 +0200

    Added a Image and Script to change the color of the image randomly when pressing space

---
+++ MEP_Project1/Assets/CountHovers.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountHovers : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            print(hit.collider.name);
+        }
+    }
+}
--- MEP_Project1/Assets/SwitchScene.cs
+++ MEP_Project1/Assets/SwitchScene.cs
@@ -8,13 +8,6 @@ public class SwitchScene : MonoBehaviour
 {
     string sceneName;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         Scene currentScene = SceneManager.GetActiveScene();
